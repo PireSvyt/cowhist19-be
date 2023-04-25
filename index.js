0626050f56485412710ae7ddc449490265a02f44
pst <81512700+PireSvyt@users.noreply.github.com>
2023-04-25 14:32:45 +0000

    Update .env and index.js

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const jwt = require("jsonwebtoken");
 
 const userRoutes = require("./src/routes/user");
 
-console.log("DB_URL : " + process.env.DB_URL);
-
 // CONNECT MONGO
 mongoose
   .connect(
     "mongodb+srv://savoyatp:" +
       process.env.DB_PW +
       "@" +
-      process.env.DB_URL +
+      process.env.DB_CLUSTER +
       "?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
