618ebee89dcab57bf2131d2829c7638e27de39b5
pst <81512700+PireSvyt@users.noreply.github.com>
2023-04-28 14:33:50 +0000

    Update auth.js and auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,7 @@ const authCtrl = require("../controllers/auth");
 
 router.post("/signup", authCtrl.signup);
 router.post("/login", authCtrl.login);
+router.post("/assess", authCtrl.login);
 
 router.post("/requesttoken", authCtrl.requesttoken);
 router.post("/resetpw", authCtrl.resetpw);
