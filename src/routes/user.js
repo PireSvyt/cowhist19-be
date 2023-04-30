5f87b68c473f2984671e84f930fc8d8eca03990f
pst <81512700+PireSvyt@users.noreply.github.com>
2023-04-30 09:07:40 +0000

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,7 +9,7 @@ router.post("/anonymize", authCtrl.authenticate, userCtrl.anonymize);
 router.post("/changepw", authCtrl.authenticate, userCtrl.changepw);
 router.post("/changelogin", authCtrl.authenticate, userCtrl.changelogin);
 router.post("/merge", authCtrl.authenticate, userCtrl.merge);
-router.get("/tables/:id", authCtrl.authenticate, userCtrl.tables);
+router.get("/tables", authCtrl.authenticate, userCtrl.tables);
 router.get("/stats/:id", authCtrl.authenticate, userCtrl.stats);
 router.get("/:id", authCtrl.authenticate, userCtrl.details);
 
