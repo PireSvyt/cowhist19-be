020b644f30eee7528962ab0d1069de1fd39856dc
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2023-05-01 21:14:53 +0000

    no await

--- src/controllers/table.js
+++ src/controllers/table.js
@@ -207,14 +207,23 @@ exports.delete = (req, res, next) => {
 async function enrichedUser (userid) {
   console.log("table.enrichedUser");
 
-  let user = await User.findOne({ _id: userid })
-  console.log(">> USER PSEUDO " + user.pseudo);
-  res({
-    _id : user._id, 
-    pseudo : user.pseudo, 
-    login : user.login,
-    status : user.status
+  User.findOne({ _id: userid }).then((user) => {
+    console.log(">> USER PSEUDO " + user.pseudo);
+    res({
+      _id : user._id, 
+      pseudo : user.pseudo, 
+      login : user.login,
+      status : user.status
+    })
   })
+  .catch((error) => {
+    res.status(400).json({
+      status: 400,
+      message: "error on find",
+      error: error,
+    });
+    console.error(error);
+  });
     
 }
 
