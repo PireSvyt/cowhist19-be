3d0629091ab5e32d76c59cbd60415b7e7060b2da
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2023-05-03 06:46:04 +0000

    remove collection

--- src/models/User.js
+++ src/models/User.js
@@ -22,4 +22,4 @@ const userSchema = mongoose.Schema(
 
 userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model("User", userSchema, "User");
+module.exports = mongoose.model("User", userSchema);
