ab017e361362b4b78e0f7c54c125d5a076125c53
pst <81512700+PireSvyt@users.noreply.github.com>
2023-05-06 21:04:14 +0000

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authCtrl = require("../controllers/auth");
 
 router.post("/signup", authCtrl.signup);
-router.post("/activate", authCtrl.activate);
+router.post("/activate/:id", authCtrl.activate);
 router.post("/login", authCtrl.login);
 router.post("/assess", authCtrl.assess);
 
