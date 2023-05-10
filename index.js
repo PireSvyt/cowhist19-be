e416ed9d5a31032d89012e9530b8cee3fb9f018d
pst <81512700+PireSvyt@users.noreply.github.com>
2023-05-10 15:25:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,45 +8,47 @@ const userRoutes = require("./src/routes/user.js");
 const gameRoutes = require("./src/routes/game.js");
 const tableRoutes = require("./src/routes/table.js");
 
-// CONNECT MONGO
-let DB_URL =
-  "mongodb+srv://savoyatp:" +
-  process.env.DB_PW +
-  "@" +
-  process.env.DB_CLUSTER +
-  "?retryWrites=true&w=majority";
-mongoose
-  .connect(DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connexion à MongoDB réussie"))
-  .catch((err) => {
-    console.log("Connexion à MongoDB échouée");
-    console.log(err);
+module.exports = function index() {
+  // CONNECT MONGO
+  let DB_URL =
+    "mongodb+srv://savoyatp:" +
+    process.env.DB_PW +
+    "@" +
+    process.env.DB_CLUSTER +
+    "?retryWrites=true&w=majority";
+  mongoose
+    .connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connexion à MongoDB réussie"))
+    .catch((err) => {
+      console.log("Connexion à MongoDB échouée");
+      console.log(err);
+    });
+
+  // CAPTURE REQ BODY
+  app.use(express.json());
+
+  // CORS MANAGEMENT
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", process.env.SUPPORTED_ORIGIN);
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
   });
 
-// CAPTURE REQ BODY
-app.use(express.json());
+  // ROUTES
+  app.use("/auth", authRoutes);
+  app.use("/user", userRoutes);
+  app.use("/game", gameRoutes);
+  app.use("/table", tableRoutes);
 
-// CORS MANAGEMENT
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.SUPPORTED_ORIGIN);
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
-});
-
-// ROUTES
-app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
-app.use("/game", gameRoutes);
-app.use("/table", tableRoutes);
-
-// Landing
-app.get("/", (req, res) => {
-  res.send("<h1>Cowhist19</h1>");
-});
+  // Landing
+  app.get("/", (req, res) => {
+    res.send("<h1>Cowhist19</h1>");
+  });
+};
