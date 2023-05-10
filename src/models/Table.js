4054d8e9d850bcea385733db48995885dccc42a8
pst <81512700+PireSvyt@users.noreply.github.com>
2023-05-10 15:03:42 +0000

    Update 5 files

--- src/models/Table.js
+++ src/models/Table.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-if (process.env.MONGOOSE_DEBUG === true) {
+/*if (process.env.MONGOOSE_DEBUG === true) {
   mongoose.set("debug", true);
-}
+}*/
 
-const tableSchema = mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  name: { type: String, required: false, unique: true },
-  users: {
-    type: [
-      {
-        type: String,
-      },
-    ],
+const tableSchema = mongoose.Schema(
+  {
+    id: { type: String, required: true, unique: true },
+    name: { type: String, required: false, unique: true },
+    users: {
+      type: [
+        {
+          type: String,
+        },
+      ],
+    },
   },
-});
+  { strict: true }
+);
 
 tableSchema.plugin(uniqueValidator);
 
