d4d8342839a85f95fd986894990bc4b22d655892
pst <81512700+PireSvyt@users.noreply.github.com>
2023-05-25 19:30:44 +0000

    Update serviceCheckAccess.js

--- src/controllers/table/services/serviceCheckAccess.js
+++ src/controllers/table/services/serviceCheckAccess.js
@@ -14,41 +14,43 @@ module.exports = async function serviceCheckAccess(tableid, authHeader) {
 
   console.log("table.serviceCheckAccess");
 
-  // Get user id from header
-  const token = authHeader && authHeader.split(" ")[1];
-  const decodedtoken = jwt_decode(token);
-  const userid = decodedtoken.id;
+  return new Promise(() => {
+    // Get user id from header
+    const token = authHeader && authHeader.split(" ")[1];
+    const decodedtoken = jwt_decode(token);
+    const userid = decodedtoken.id;
 
-  // Find table
-  Table.findOne({ _id: tableid })
-    .then((table) => {
-      if (table !== undefined) {
-        let userList = table.users.filter((user) => {
-          return user._id === userid;
-        });
-        if (userList.length !== 0) {
-          return {
-            outcome: true,
-            reason: "table.ismember",
-          };
+    // Find table
+    Table.findOne({ _id: tableid })
+      .then((table) => {
+        if (table !== undefined) {
+          let userList = table.users.filter((user) => {
+            return user._id === userid;
+          });
+          if (userList.length > 0) {
+            return {
+              outcome: true,
+              reason: "table.ismember",
+            };
+          } else {
+            return {
+              outcome: false,
+              reason: "table.notamember",
+            };
+          }
         } else {
           return {
             outcome: false,
-            reason: "table.notamember",
+            reason: "table.notfound",
           };
         }
-      } else {
+      })
+      .catch((error) => {
+        console.error(error);
         return {
           outcome: false,
-          reason: "table.notfound",
+          reason: "table.erroronfind",
         };
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      return {
-        outcome: false,
-        reason: "table.erroronfind",
-      };
-    });
+      });
+  });
 };
