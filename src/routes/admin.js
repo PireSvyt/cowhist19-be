3c2907987c3e9b653b1e20715781ca5fff79fcf5
pst <81512700+PireSvyt@users.noreply.github.com>
2023-05-26 22:07:58 +0000

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,7 +5,7 @@ const authAuthenticate = require("../controllers/auth/authAuthenticate.js");
 const tablesByGames = require("../controllers/admin/tablesByGames.js");
 const usersByStatus = require("../controllers/admin/usersByStatus.js");
 
-router.get("/v1/tablebygames", authAuthenticate, tablesByGames);
+router.get("/v1/tablesbygames", authAuthenticate, tablesByGames);
 router.get("/v1/usersbystatus", authAuthenticate, usersByStatus);
 
 module.exports = router;
