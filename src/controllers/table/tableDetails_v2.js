d2597ce84fbbec01f5c7a28a637e6b1163f3a68f
pst <81512700+PireSvyt@users.noreply.github.com>
2023-06-11 10:33:23 +0000

    Update tableDetails_v2.js

--- src/controllers/table/tableDetails_v2.js
+++ src/controllers/table/tableDetails_v2.js
@@ -69,7 +69,6 @@ module.exports = tableDetails_v2 = (req, res, next) => {
               for (var guest = 1; guest <= table.guests; guest++) {
                 table.players.push({
                   _id: random_id(),
-                  pseudo: "guest",
                   status: "guest",
                 });
               }
