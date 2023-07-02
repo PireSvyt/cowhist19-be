c059cc873ae016f248914ade2ff9cdda606e6bf9
pst <81512700+PireSvyt@users.noreply.github.com>
2023-07-02 18:09:05 +0000

    Update feedback.js

--- src/routes/feedback.js
+++ src/routes/feedback.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const authAuthenticate = require("../controllers/auth/authAuthenticate.js");
 const feedbackCreate = require("../controllers/feedback/feedbackCreate.js");
 
-router.post("/v1/feedback", authAuthenticate, feedbackCreate);
+router.post("/v1/create", authAuthenticate, feedbackCreate);
 
 module.exports = router;
