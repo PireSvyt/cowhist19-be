a5f77e66579f3b27d7b0a49885b9eab523e65b9b
pst <81512700+PireSvyt@users.noreply.github.com>
2023-07-05 04:48:00 +0000

    Update objectCount.js

--- src/controllers/admin/objectCount.js
+++ src/controllers/admin/objectCount.js
@@ -1,6 +1,7 @@
 const User = require("../../models/User.js");
 const Table = require("../../models/Table.js");
 const Game = require("../../models/Game.js");
+const Feedback = require("../../models/Feedback.js");
 const serviceCheckAdmin = require("./services/serviceCheckAdmin.js");
 
 module.exports = objectCount = (req, res, next) => {
@@ -14,6 +15,7 @@ module.exports = objectCount = (req, res, next) => {
   * admin.objectcount.error.oncountusers
   * admin.objectcount.error.oncounttables
   * admin.objectcount.error.oncountgames
+  * admin.objectcount.error.oncountfeedbacks
   
   */
 
@@ -34,14 +36,25 @@ module.exports = objectCount = (req, res, next) => {
             .then((tables) => {
               Game.count()
                 .then((games) => {
-                  res.status(200).json({
-                    type: "admin.objectcount.success",
-                    data: {
-                      users: users,
-                      tables: tables,
-                      games: games,
-                    },
-                  });
+                  Feedback.count()
+                    .then((feedbacks) => {
+                      res.status(200).json({
+                        type: "admin.objectcount.success",
+                        data: {
+                          users: users,
+                          tables: tables,
+                          games: games,
+                          feedbacks: feedbacks,
+                        },
+                      });
+                    })
+                    .catch((error) => {
+                      res.status(400).json({
+                        type: "admin.objectcount.error.oncountfeedbacks",
+                        error: error,
+                      });
+                      console.error(error);
+                    });
                 })
                 .catch((error) => {
                   res.status(400).json({
