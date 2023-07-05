32d0b1d3e79734151c1b4bd664d551cf12e7ba77
pst <81512700+PireSvyt@users.noreply.github.com>
2023-07-05 04:35:12 +0000

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -13,7 +13,7 @@ const feedbackClose = require("../controllers/admin/feedbackClose.js");
 router.get("/v1/tablesbyplayers", authAuthenticate, tablesByPlayers);
 router.get("/v1/tablesbygames", authAuthenticate, tablesByGames);
 router.get("/v1/usersbystatus", authAuthenticate, usersByStatus);
-router.get("/v1/objectCount", authAuthenticate, objectCount);
+router.get("/v1/objectcount", authAuthenticate, objectCount);
 router.post("/v1/populate", authAuthenticate, populate);
 router.post("/v1/feedbacklist", authAuthenticate, feedbackList);
 router.post("/v1/feedbackclose", authAuthenticate, feedbackClose);
