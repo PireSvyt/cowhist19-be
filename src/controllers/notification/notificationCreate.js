46a532885a5b80a9911ee784e4ded4e944354e41
pst <81512700+PireSvyt@users.noreply.github.com>
2023-07-18 05:29:23 +0000

    Update notificationCreate.js and notification.js

--- src/controllers/notification/notificationCreate.js
+++ src/controllers/notification/notificationCreate.js
@@ -15,39 +15,27 @@ module.exports = notificationCreate = (req, res, next) => {
 
   console.log("notification.create");
 
-  // Check access
-  serviceCheckAdmin(req.headers["authorization"]).then((access) => {
-    if (!access.outcome) {
-      // Unauthorized
-      res.status(401).json({
-        type: "notification.create.error.deniedaccess",
-        error: access.reason,
+  const notificationToSave = new Notification({ ...req.body });
+  notificationToSave.id = notificationToSave._id;
+  // Save
+  notificationToSave
+    .save()
+    .then(() => {
+      res.status(201).json({
+        type: "notification.create.success",
+        data: {
+          id: notificationToSave._id,
+        },
       });
-    } else {
-      // Create
-      const notificationToSave = new Notification({ ...req.body });
-      notificationToSave.id = notificationToSave._id;
-      // Save
-      notificationToSave
-        .save()
-        .then(() => {
-          res.status(201).json({
-            type: "notification.create.success",
-            data: {
-              id: notificationToSave._id,
-            },
-          });
-        })
-        .catch((error) => {
-          res.status(400).json({
-            type: "notification.create.error.oncreate",
-            error: error,
-            data: {
-              id: "",
-            },
-          });
-          console.error(error);
-        });
-    }
-  });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        type: "notification.create.error.oncreate",
+        error: error,
+        data: {
+          id: "",
+        },
+      });
+      console.error(error);
+    });
 };
